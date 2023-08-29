88cab24ff7b7255478bc52f48bdc288c6836b036
Aryan Sabet Nejad <74782534+aryansabet@users.noreply.github.com>
2023-08-29 09:39:13 +0330

    feat: add useDispatch & useSelector

--- src/features/icecream/IcecreamView.jsx
+++ src/features/icecream/IcecreamView.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-
+import { useSelector, useDispatch } from "react-redux";
+import { ordered,restocked } from "./icecreamSlice";
 export const IcecreamView = () => {
+  const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams);
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <h2>Number of IceCreams</h2>
-      <button>Order IceCream</button>
-      <button>Restock IceCreams</button>
+      <h2>Number of IceCreams - {numOfIcecreams}</h2>
+      <button onClick={() => dispatch(ordered())}>Order IceCream</button>
+      <button onClick={() => dispatch(restocked(5))}>Restock IceCreams</button>
     </div>
   );
 };
--- src/features/icecream/icecreamSlice.js
+++ src/features/icecream/icecreamSlice.js
@@ -5,7 +5,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import {ordered as cakeOrdered} from "../cake/cakeSlice"
 
 const initialState = {
-  numOfIcecreams: 10,
+  numOfIcecreams: 20,
 };
 const icecreamSlice = createSlice({
   name: "icecream",
